dc973cb2ba484ae38c995b257cfc2b9d132881ad
pravin-nawghare <nawgharepraveen@gmail.com>
2025-03-04 16:40:48 +0530

    requirements file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'churn-prediction',
+    version = "0.0.1",
+    author = "Pravin",
+    author_email = "nawgharepravin0@gmial.com",
+    packages = find_packages()
+)
